7799dedb88a7ebe4998ac357d3832d629231ae1c
oleguswild <dragooj@yandex.ru>
2023-01-20 13:51:26 +0300

    Add some hardcoding to display new properties In Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,4 +10,13 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
 
+    list_display = ['email', 'username', 'age', 'is_staff', ] 
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+                                        )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+                                        ) 
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
