8697f9c4c03dcbab27c5ef72d4f9d78369f2ef34
oleguswild <dragooj@yandex.ru>
2023-01-21 14:09:45 +0300

    Add ability to modify user info (no permissions checked)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, UserUpdateView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('update/<int:pk>', UserUpdateView.as_view(), name='user_update')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.views.generic import CreateView
+from django.views.generic import UpdateView
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class UserUpdateView(UpdateView):
+    model = CustomUser
+    fields = ['age', 'email']
+    success_url = reverse_lazy('home')  # TODO change to detailed view
+    template_name = 'registration/user_update.html'
