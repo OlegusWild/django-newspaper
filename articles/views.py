40c577e6ee2d897eeaccf4fc70cadb45fcfedbbd
oleguswild <dragooj@yandex.ru>
2023-01-23 15:43:26 +0300

    Restricted access to artcls for non-authenticated (LoginRequiredMixin)
    Used form_valid View method to auto set author
    
    Bug at the home page when non-auth user

--- articles/views.py
+++ articles/views.py
@@ -1,27 +1,35 @@
 from django.views.generic import ListView, DeleteView, DetailView, UpdateView, CreateView
 from .models import Article
 from django.urls import reverse_lazy
+from django.forms import BaseModelForm
+from django.shortcuts import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
     template_name = 'article_create.html'
 
-class ArticleUpdateView(UpdateView):
+    # Do not offer to choose any user
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user  # setting actual logged-in user
+        return super().form_valid(form)
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ('title', 'body')
     template_name = 'article_edit.html'
